2b54c28281b74c6ae547f56c2f7bd33c4ae9f2ce
Nayan <nayan@appsmith.com>
2021-06-11 02:02:22 +0600

    -added pagination to notifications API

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/NotificationService.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/NotificationService.java
@@ -1,7 +1,13 @@
 package com.appsmith.server.services;
 
 import com.appsmith.server.domains.Notification;
+import com.appsmith.server.dtos.ResponseDTO;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestParam;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 public interface NotificationService extends CrudService<Notification, String> {
-
+    Mono<ResponseDTO<List<Notification>>> getAll(@RequestParam MultiValueMap<String, String> params);
 }
