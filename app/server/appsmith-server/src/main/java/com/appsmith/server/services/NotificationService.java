7bc5fe54d4b10590422ff72749b960a42d0024e0
Nayan <nayan@appsmith.com>
2021-06-11 18:09:02 +0600

    -added unread count in get notifications API

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/NotificationService.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/NotificationService.java
@@ -1,13 +1,12 @@
 package com.appsmith.server.services;
 
 import com.appsmith.server.domains.Notification;
-import com.appsmith.server.dtos.ResponseDTO;
+import com.appsmith.server.dtos.NotificationsResponseDTO;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestParam;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
-
 public interface NotificationService extends CrudService<Notification, String> {
-    Mono<ResponseDTO<List<Notification>>> getAll(@RequestParam MultiValueMap<String, String> params);
+    Mono<NotificationsResponseDTO> getAll(@RequestParam MultiValueMap<String, String> params);
+
 }
